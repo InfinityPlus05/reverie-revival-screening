db979f1c4b60d4766eb148ae9d6469ff8be4d2b8
InfinityPlus05 <piercove2005@gmail.com>
2026-06-01 20:09:15 -0400

    edited icon

---
+++ data/atlas.lua
@@ -0,0 +1,50 @@
+Reverie.atlases = {
+    {
+        key = "modicon",
+        path = "icon.png",
+        px = 34,
+        py = 34
+    },
+    {
+        key = "Cine",
+        path = "cines.png",
+        px = 71,
+        py = 95
+    },
+    {
+        key = "cine_jokers",
+        path = "jokers.png",
+        px = 71,
+        py = 95
+    },
+    {
+        key = "cine_boosters",
+        path = "boosters.png",
+        px = 71,
+        py = 95
+    },
+    {
+        key = "cine_vouchers",
+        path = "vouchers.png",
+        px = 71,
+        py = 95
+    },
+    {
+        key = "cine_tags",
+        path = "tags.png",
+        px = 34,
+        py = 34
+    },
+    {
+        key = "cine_backs",
+        path = "backs.png",
+        px = 71,
+        py = 95
+    },
+    {
+        key = "cine_sleeves",
+        path = "sleeves.png",
+        px = 73,
+        py = 95
+    }
+}
